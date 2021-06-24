4c88372cab79b49edb7490fb89b649b0581ec911
Kejia Qiang <kqiang@uci.edu>
2021-06-24 11:20:05 +0800

    daily update

--- Render.py
+++ Render.py
@@ -6,4 +6,10 @@ if __name__ == '__main__':
     print("Start Running...")
     obj = OBJ()
     obj.parse_from_file("D:\\ML\\cube.obj")
-    print(obj)
+    
+    window = tk.Tk()
+    window.geometry("800x600")
+    window.title("Simple Render")
+    lbl = tk.Label(window, text=f"Now open: {obj.name}")
+    lbl.grid(column=0, row=0)
+    window.mainloop()
